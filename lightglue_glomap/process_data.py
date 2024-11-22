90ff227ab8f2baeda9ac775ba26e5ff41fcd860e
pablovela5620 <pablovela5620@gmail.com>
2024-11-21 18:13:49 -0600

    add rerun logging for features + matches

--- lightglue_glomap/process_data.py
+++ lightglue_glomap/process_data.py
@@ -9,7 +9,141 @@ from hloc.triangulation import (
     import_matches,
     estimation_and_geometric_verification,
 )
+from hloc.utils.io import get_keypoints, get_matches
+from tqdm import tqdm
 from timeit import default_timer as timer
+
+import rerun as rr
+import cv2
+import numpy as np
+
+
+def log_features(
+    bgr_dict: dict[str, np.ndarray],
+    features: Path,
+    matching_method: Literal["vocab_tree", "exhaustive", "sequential"],
+    max_dim: int = 640,
+) -> None:
+    # log keypoints
+    for idx, (image_name, bgr) in enumerate(
+        tqdm(bgr_dict.items(), desc="Logging keypoints")
+    ):
+        rr.set_time_sequence("features", idx)
+        cam_log_path: str = (
+            "camera" if matching_method == "sequential" else f"{image_name}"
+        )
+        keypoints = get_keypoints(features, image_name)
+
+        # reshape bgr and keypoints to vga like resolution max dim 640
+        scale = max_dim / max(bgr.shape[:2])
+        bgr = cv2.resize(bgr, (0, 0), fx=scale, fy=scale)
+        keypoints = keypoints * scale
+
+        rr.log(
+            f"{cam_log_path}/image",
+            rr.Image(bgr, color_model=rr.ColorModel.BGR).compress(jpeg_quality=50),
+        )
+        rr.log(f"{cam_log_path}/image/keypoints", rr.Points2D(positions=keypoints))
+
+
+def log_matches(
+    bgr_dict: dict[str, np.ndarray],
+    pairs_path: Path,
+    matches_path: Path,
+    features_path: Path,
+    image_ids: dict[str, int],
+) -> None:
+    with open(str(pairs_path), "r") as f:
+        pairs = [p.split() for p in f.readlines()]
+
+    matched = set()
+    cam_log_path = "camera"
+    # matched_images = []
+
+    rr.reset_time()  # Clears all set timeline info.
+    for idx, (name0, name1) in enumerate(
+        tqdm(pairs, desc="Logging matches", total=len(pairs))
+    ):
+        id0, id1 = image_ids[name0], image_ids[name1]
+        if len({(id0, id1), (id1, id0)} & matched) > 0:
+            continue
+        rr.set_time_sequence("matches", idx)
+        # load matches
+        matches, scores = get_matches(matches_path, name0, name1)
+
+        # load keypoints
+        keypoints0 = get_keypoints(features_path, name0)[matches[:, 0]]
+        keypoints1 = get_keypoints(features_path, name1)[matches[:, 1]]
+        bgr0 = bgr_dict[name0]
+        bgr1 = bgr_dict[name1]
+        h0, w0, _ = bgr0.shape
+        h1, w1, _ = bgr1.shape
+        # merge images horizontally
+        bgr = cv2.hconcat([bgr0, bgr1])
+        keypoints1[:, 0] += w0
+
+        # resize to vga like resolution max dim 640
+        max_dim = 640
+        scale = max_dim / max(h0, w0, h1, w1)
+        bgr = cv2.resize(bgr, (0, 0), fx=scale, fy=scale)
+        keypoints0 *= scale
+        keypoints1 *= scale
+
+        line_segments = np.stack([keypoints0, keypoints1], axis=1)  # Shape: (n, 2, 2)
+        # matched_images.append(bgr)
+
+        # draw matches
+        rr.log(
+            f"{cam_log_path}/image",
+            rr.Image(bgr, color_model=rr.ColorModel.BGR).compress(jpeg_quality=25),
+        )
+        rr.log(f"{cam_log_path}/image/keypoints0", rr.Points2D(positions=keypoints0))
+        rr.log(f"{cam_log_path}/image/keypoints1", rr.Points2D(positions=keypoints1))
+        rr.log(
+            f"{cam_log_path}/image/matches",
+            rr.LineStrips2D(strips=line_segments, colors=(0, 255, 0), radii=0.25),
+        )
+
+        matched |= {(id0, id1), (id1, id0)}
+    # only sends uncompressed images
+    # send_data_columns(matched_images=matched_images, cam_log_path=cam_log_path)
+
+
+def send_data_columns(matched_images: np.ndarray, cam_log_path: str) -> None:
+    # Timeline on which the images are distributed.
+    start = timer()
+    matched_images = np.stack(matched_images)
+
+    sequence = np.arange(0, len(matched_images))
+    # Log the ImageFormat and indicator once, as static.
+
+    format_static = rr.components.ImageFormat(
+        width=matched_images.shape[2],
+        height=matched_images.shape[1],
+        color_model="BGR",
+        channel_datatype="U8",
+    )
+    rr.log(
+        f"{cam_log_path}/image",
+        [format_static, rr.Image.indicator()],
+        static=True,
+    )
+
+    # Send all images at once.
+    rr.send_columns(
+        f"{cam_log_path}/image",
+        times=[rr.TimeSequenceColumn("matches", sequence)],
+        # Reshape the images so `ImageBufferBatch` can tell that this is several blobs.
+        #
+        # Note that the `ImageBufferBatch` consumes arrays of bytes,
+        # so if you have a different channel datatype than `U8`, you need to make sure
+        # that the data is converted to arrays of bytes before passing it to `ImageBufferBatch`.
+        components=[
+            rr.components.ImageBufferBatch(matched_images.reshape(len(sequence), -1))
+        ],
+    )
+
+    print(f"Time taken to log matches: {timer() - start:.2f} seconds")
 
 
 def run_lightglue_glomap(
@@ -19,23 +153,22 @@ def run_lightglue_glomap(
     verbose: bool = False,
     matching_method: Literal["vocab_tree", "exhaustive", "sequential"] = "vocab_tree",
     feature_type: Literal[
-        "sift",
-        "superpoint_aachen",
-        "disk",
+        "sift", "superpoint_aachen", "disk", "xfeat"
     ] = "superpoint_aachen",
     matcher_type: Literal[
         "superglue",
         "NN-ratio",
         "NN-mutual",
         "disk+lightglue",
+        "superpoint+lightglue",
     ] = "superglue",
     num_matched: int = 50,
     use_single_camera_mode: bool = True,
+    colmap_cmd: Literal["colmap", "glomap"] = "glomap",
 ) -> None:
     """Runs hloc on the images.
 
     Args:
-        image_dir: Path to the directory containing the images.
         colmap_dir: Path to the output directory.
         camera_model: Camera model to use.
         verbose: If True, logs the output of the command.
@@ -54,33 +187,54 @@ def run_lightglue_glomap(
         pairs_from_retrieval,
     )
 
+    # if colmap_dir.exists() and colmap_dir.name == "test":
+    #     print("Removing existing colmap_dir")
+    #     import shutil
+
+    #     shutil.rmtree(colmap_dir)
+
     outputs = colmap_dir
     sfm_pairs = outputs / "pairs-netvlad.txt"
     sfm_dir = outputs / "sparse" / "0"
     features = outputs / "features.h5"
     matches = outputs / "matches.h5"
 
+    # load bgr images into a dict for logging
+    bgr_dict: dict[str, np.ndarray] = {}
+    for img_path in sorted(image_dir.iterdir()):
+        bgr_dict[img_path.name] = cv2.imread(str(img_path))
+
     retrieval_conf = extract_features.confs["netvlad"]  # type: ignore
     feature_conf = extract_features.confs[feature_type]  # type: ignore
     matcher_conf = match_features.confs[matcher_type]  # type: ignore
 
-    references = [p.relative_to(image_dir).as_posix() for p in image_dir.iterdir()]
+    references: list[str] = sorted(
+        [p.relative_to(image_dir).as_posix() for p in image_dir.iterdir()]
+    )
 
-    start_time = timer()
+    start_time: float = timer()
 
     extract_features.main(
         feature_conf, image_dir, image_list=references, feature_path=features
     )  # type: ignore
+
+    log_features(bgr_dict=bgr_dict, features=features, matching_method=matching_method)
+
     if matching_method == "exhaustive":
         pairs_from_exhaustive.main(sfm_pairs, image_list=references)  # type: ignore
     elif matching_method == "sequential":
         pairs_from_sequential.main(
-            output=sfm_pairs, image_list=references, features=features, overlap=10
+            output=sfm_pairs,
+            image_list=references,
+            features=features,
+            overlap=10,
+            quadratic_overlap=False,
         )  # type: ignore
     else:
         retrieval_path = extract_features.main(retrieval_conf, image_dir, outputs)  # type: ignore
         num_matched = min(len(references), num_matched)
         pairs_from_retrieval.main(retrieval_path, sfm_pairs, num_matched=num_matched)  # type: ignore
+
     match_features.main(matcher_conf, sfm_pairs, features=features, matches=matches)  # type: ignore
 
     image_options = pycolmap.ImageReaderOptions(camera_model=camera_model.value)  # type: ignore
@@ -101,7 +255,16 @@ def run_lightglue_glomap(
     import_images(
         image_dir, database, camera_mode, image_list=None, options=image_options
     )
-    image_ids = get_image_ids(database)
+    image_ids: dict[str, int] = get_image_ids(database)
+
+    log_matches(
+        bgr_dict=bgr_dict,
+        pairs_path=sfm_pairs,
+        matches_path=matches,
+        features_path=features,
+        image_ids=image_ids,
+    )
+
     import_features(image_ids, database, features)
     import_matches(
         image_ids,
@@ -113,8 +276,8 @@ def run_lightglue_glomap(
     )
     estimation_and_geometric_verification(database, sfm_pairs, verbose)
 
-    colmap_cmd = "glomap"
     # Bundle adjustment
+    num_images: int = len(list(image_dir.glob("*")))
     sparse_dir = colmap_dir / "sparse"
     sparse_dir.mkdir(parents=True, exist_ok=True)
     mapper_cmd = [
@@ -123,6 +286,10 @@ def run_lightglue_glomap(
         f"--image_path {image_dir}",
         f"--output_path {sparse_dir}",
     ]
+    if colmap_cmd == "glomap":
+        mapper_cmd += [
+            f"--TrackEstablishment.max_num_tracks {2000*num_images}",
+        ]
 
     mapper_cmd = " ".join(mapper_cmd)
 
--- tools/reconstruct.py
+++ tools/reconstruct.py
@@ -2,24 +2,30 @@ from pathlib import Path
 from lightglue_glomap.process_data import run_lightglue_glomap
 from lightglue_glomap.colmap_utils import CameraModel
 from argparse import ArgumentParser
+import rerun as rr
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
+    name = "reconstruction-lightglue-glomap"
+    parser = ArgumentParser(name)
     parser.add_argument("--image-dir", type=Path, required=True)
+    rr.script_add_args(parser)
     args = parser.parse_args()
+    rr.script_setup(args, application_id=f"{name}")
 
     images_dir: Path = args.image_dir
     assert images_dir.exists(), images_dir
 
     run_lightglue_glomap(
         image_dir=images_dir,
-        colmap_dir=images_dir.parent / "colmap",
+        colmap_dir=images_dir.parent / "test",
         camera_model=CameraModel.OPENCV,
         verbose=True,
         matching_method="sequential",
-        feature_type="superpoint_aachen",
-        matcher_type="superglue",
+        feature_type="xfeat",
+        matcher_type="NN-mutual",
         num_matched=50,
         use_single_camera_mode=True,
+        colmap_cmd="glomap",
     )
+    rr.script_teardown(args)
